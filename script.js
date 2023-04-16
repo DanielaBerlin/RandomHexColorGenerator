7215efd4381f984194d51944e068948a0c5bd859
Daniela <danielacardona2203@gmail.com>
2023-04-16 23:06:04 +0200

    input character project

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-const hex = document.querySelector(".hex");
-const btn = document.querySelector(".generate");
+const count = document.querySelector(".count");
+const input = document.querySelector("input");
 
-const generateColor = () => {
-    const randomColor = Math.random().toString(16).substring(2, 8);
-    document.body.style.backgroundColor = "#" + randomColor;
-    hex.innerHTML = "#" + randomColor;
-};
-
-btn.addEventListener("click", generateColor);
-
-generateColor();
-
-// let color = Math.random();
-// color = Math.random().toString(16);
-// color = Math.random().toString(16).substring(2, 8);
-
-
-// console.log(color);
+input.addEventListener("keyup", () => {
+    count.innerHTML = input.value.length;
+});
