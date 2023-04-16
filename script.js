95cff87c458cc6ac5edd259da302cf0a85417d33
Daniela <danielacardona2203@gmail.com>
2023-04-16 23:16:11 +0200

    generatedColor function added

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-const count = document.querySelector(".count");
-const input = document.querySelector("input");
+const hex = document.querySelector(".hex");
+const btn = document.querySelector(".generate");
 
-input.addEventListener("keyup", () => {
-    count.innerHTML = input.value.length;
-});
+const generateColor = () => {
+    const randomColor = Math.random().toString(16).substring(2, 8);
+    document.body.style.backgroundColor = "#" + randomColor;
+    hex.innerHTML = "#" + randomColor;
+};
+
+btn.addEventListener("click", generateColor);
+
+generateColor();
+
+// let color = Math.random();
+// color = Math.random().toString(16);
+// color = Math.random().toString(16).substring(2, 8);
+
+
+// console.log(color);
